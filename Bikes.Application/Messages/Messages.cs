c2429b5d59eb96a3fbc819629ef4d924eb94611a
Ala <124534654+AlaML432@users.noreply.github.com>
2025-01-06 16:08:04 +0100

    Add files via upload

---
+++ Bikes.Application/Messages/Messages.cs
@@ -0,0 +1,33 @@
+﻿using Bikes.Domain;
+using Microsoft.EntityFrameworkCore;
+using Bikes.Infrastructure;
+
+namespace Bikes.API.Services
+{
+    public class MessageService
+    {
+        private readonly DataContext _context;
+
+        public MessageService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Zapisanie wiadomości
+        public async Task SendMessageAsync(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+        }
+
+        // Pobranie wiadomości dla administratora
+        public async Task<List<Message>> GetMessagesForAdminAsync()
+        {
+            // Filtrujemy wiadomości, które są wysłane do admina
+            return await _context.Messages
+                                 .Where(m => m.ReceiverId == "Admin")
+                                 .OrderByDescending(m => m.SentDate)
+                                 .ToListAsync();
+        }
+    }
+}
