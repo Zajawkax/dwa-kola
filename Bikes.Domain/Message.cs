074428ed5b7c4626316d54e2e7ee551c331dbf8e
Ala <124534654+AlaML432@users.noreply.github.com>
2025-01-06 18:09:41 +0100

    Add files via upload

---
+++ Bikes.Domain/Message.cs
@@ -0,0 +1,13 @@
+﻿using Bikes.Domain;
+
+public class Message
+{
+    public int MessageId { get; set; }
+    public string Content { get; set; }
+    public DateTime SentDate { get; set; }
+
+    // Powiązanie z użytkownikiem (nazwa użytkownika jako autor)
+    public string SenderId { get; set; }
+    public string ReceiverId { get; set; } // Zawsze "Admin" dla administratora
+    public User User { get; set; }
+}
