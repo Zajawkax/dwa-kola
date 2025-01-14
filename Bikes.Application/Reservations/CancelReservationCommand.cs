55469261230c7f8674cec07cab06c9f3fe55b7f8
wiking73 <pawlikowskaw@op.pl>
2025-01-14 18:02:51 +0100

    Admin Panel Rezerwacji

---
+++ Bikes.Application/Reservations/CancelReservationCommand.cs
@@ -0,0 +1,50 @@
+﻿using Bikes.Application.Core;
+using Bikes.Domain;
+using Bikes.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikes.Application.Reservations
+{
+    public class CancelReservationCommand : IRequest<Result<Reservation>>
+    {
+        public int ReservationId { get; set; }
+    }
+
+    public class CancelReservationHandler : IRequestHandler<CancelReservationCommand, Result<Reservation>>
+    {
+        private readonly DataContext _context;
+
+        public CancelReservationHandler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<Reservation>> Handle(CancelReservationCommand request, CancellationToken cancellationToken)
+        {
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(r => r.ReservationId == request.ReservationId, cancellationToken);
+
+            if (reservation == null)
+            {
+                return Result<Reservation>.Failure("Rezerwacja nie została znaleziona.");
+            }
+
+            reservation.Status = ReservationStatus.Cancelled;
+
+            var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result)
+            {
+                return Result<Reservation>.Failure("Nie udało się anulować rezerwacji.");
+            }
+
+            return Result<Reservation>.Success(reservation);
+        }
+    }
+}
---
+++ Bikes.Application/Reservations/ConfirmReservationCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bikes.Application.Core;
+using Bikes.Domain;
+using Bikes.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bikes.Application.Reservations
+{
+
+    public class ConfirmReservationCommand : IRequest<Result<Reservation>>
+    {
+        public int ReservationId { get; set; }
+    }
+
+    public class ConfirmReservationHandler : IRequestHandler<ConfirmReservationCommand, Result<Reservation>>
+    {
+        private readonly DataContext _context;
+
+        public ConfirmReservationHandler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<Reservation>> Handle(ConfirmReservationCommand request, CancellationToken cancellationToken)
+        {
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(r => r.ReservationId == request.ReservationId, cancellationToken);
+
+            if (reservation == null)
+            {
+                return Result<Reservation>.Failure("Rezerwacja nie została znaleziona.");
+            }
+
+            reservation.Status = ReservationStatus.Confirmed;
+
+ 
+            var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result)
+            {
+                return Result<Reservation>.Failure("Nie udało się potwierdzić rezerwacji.");
+            }
+
+            return Result<Reservation>.Success(reservation);
+        }
+    }
+}
