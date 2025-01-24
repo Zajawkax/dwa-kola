24670b539495ac52351201ce349aeb3ab6ba13c4
Ala <124534654+AlaML432@users.noreply.github.com>
2025-01-24 19:44:53 +0100

    Update Kontakt.tsx

--- Bikes.API/clientapp/src/Components/Kontakt.tsx
+++ Bikes.API/clientapp/src/Components/Kontakt.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import '../Styles/contact.css';
+import '../Styles/Contact.css';
 
 const Contact: React.FC = () => {
     const [email, setEmail] = useState('');
